e43f474338dc016e286ab8def3dfe6e28140a821
Deshawn Johal <deshawnjohal@gmail.com>
2025-04-26 18:35:21 +0100

    created type def struct to hold the main atributes of the car which then is creating a pointer to carInventory for use and to be initalised inside the .c file

--- include/Inventory.h
+++ include/Inventory.h
@@ -1,8 +1,19 @@
-//
-// Created by Deshawn Johal on 26/04/2025.
-//
-
 #ifndef INVENTORY_H
 #define INVENTORY_H
+
+//defines the size of the inital struct array
+#define INITIAL_NAME_SIZE 50
+
+//first need to create a struct for handling the specifications of the cars
+typedef struct {
+  char name[INITIAL_NAME_SIZE];
+  int year;
+  float price;
+  int stock
+} car;
+
+// points to carInventory which will be used in inventory.c file
+car* carInventory;
+
 
 #endif //INVENTORY_H
