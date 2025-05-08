6274480debf84dbcde2a3fd6b295a301198f245c
DjDesh123 <Deshawnjohal@gmail.com>
2025-05-08 19:47:53 +0100

    created the main menu logic by adding a switch case and using a do while loop to keep looping over the main menu until the user enters a valid option.
    in the header file I wrote an menuOption typedef enum for the readability of the switch case

--- src/MainMenu.c
+++ src/MainMenu.c
@@ -1,2 +1,38 @@
 #include <stdio.h>
 #include "MainMenu.h"
+
+#include <stdlib.h>
+
+
+void mainMenu() {
+    int menuChoice;
+
+    // Displays the options that the user can pick
+    printf("---- Car Dealership System ----\n");
+    printf("1. View Available Cars\n");
+    printf("2. Buy Cars\n");
+    printf("3. View Sales Stats\n");
+    printf("4. Sell Car\n");
+    printf("5. Exit\n");
+    printf("Select an option: ");
+    scanf("%d", &menuChoice);
+
+    // to loop untill the user enters a valid choice
+    do {
+        switch (menuChoice) {
+            case VIEW_CARS:
+                printf("Available Cars:\n");
+            case BUY_CAR:
+                printf("Buy Cars:\n");
+            case VIEW_STATS:
+                printf("View Sales Stats:\n");
+            case SELL_CAR:
+                printf("Sell Car:\n");
+            case EXIT:
+                exit(0);
+            default:
+                printf("Invalid Input\n");
+        }
+    }while(menuChoice<VIEW_CARS||menuChoice >EXIT);
+}
+
--- src/MainMenu.h
+++ src/MainMenu.h
@@ -2,4 +2,10 @@
 #ifndef MAINMENU_H
 #define MAINMENU_H
 
+// to improve readability we used enums
+typedef enum {VIEW_CARS = 1,BUY_CAR=2,VIEW_STATS=3,SELL_CAR=4,EXIT=5} menuOption;
+
+
+int menuChoice;
+
 #endif //MAINMENU_H
