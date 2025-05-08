c8ea97468aa292e4a9f5b95679fe649f89af4ef7
DjDesh123 <Deshawnjohal@gmail.com>
2025-05-08 20:09:16 +0100

    moved the MainMenu.h file to the include folder as its a header file

--- include/MainMenu.h
+++ include/MainMenu.h
@@ -2,6 +2,9 @@
 #ifndef MAINMENU_H
 #define MAINMENU_H
 
+// function prototype
+void mainMenu();
+
 // to improve readability we used enums
 typedef enum {VIEW_CARS = 1,BUY_CAR=2,VIEW_STATS=3,SELL_CAR=4,EXIT=5} menuOption;
 
