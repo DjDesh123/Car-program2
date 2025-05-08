06db228a1f3f26cd101b15170e9ceaa9670b5bbe
DjDesh123 <Deshawnjohal@gmail.com>
2025-05-08 20:13:31 +0100

    called the MainMenu.h file to call the mainmenu function when the program starts up. removed the inventory.c as we will eb handling it later

--- src/Inventory.c
+++ src/Inventory.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
-#include "include/inventory.h"
+
 
 //The main plan is use structs and dynamic arrays with dynamic memory to access this stuff so first i needf to create a header file and to put all this stuff in there
