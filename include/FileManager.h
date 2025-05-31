8d6e07c4bbe348ce98b2711c4aab4cc8d0e51070
DjDesh123 <Deshawnjohal@gmail.com>
2025-05-31 17:06:18 +0100

    what I have done mainly is to use timestamps for the backups so that  I know for later which is the oldest backup so when I decide to create the only 5 backups feature i can just easy locate the oldest one in the directroy and delete it to allow "room" for the next update

--- include/FileManager.h
+++ include/FileManager.h
@@ -5,6 +5,8 @@
 #include <stdio.h>
 
 // Creates a backup of the given file. Returns 0 on success, -1 on failure.
-void backupFile(const char* fileName, const char* backupFileName);
+void backupFile(const char* fileName);
+static void generateBackupFileName(const char* fileName,size_t size);
+
 
 #endif // FILE_MANAGER_H
--- src/FileManager.c
+++ src/FileManager.c
@@ -1,19 +1,24 @@
 #include "../include/FileManager.h"
 
-
+#include <time.h>
 
 
 // Handles file-related operations such as creating backups.
 // This can be called from the bootloader or other parts of the program.
 
-void backupFile(const char* fileName, const char* backupFileName) {
+void backupFile(const char* fileName) {
 
     // Open the source file in binary read mode
     FILE *file = fopen(fileName, "rb");
 
     if (file == NULL) {
         perror("Error opening source file.\n");
+        return;
     }
+
+    // creates the backup file name
+    char backupFileName[256];
+    generateBackupFileName(backupFileName, sizeof(backupFileName));
 
     // Create the backup file in binary write mode
     FILE *backupFile = fopen(backupFileName, "wb");
@@ -34,4 +39,11 @@ void backupFile(const char* fileName, const char* backupFileName) {
     fclose(file);
     fclose(backupFile);
 
+}
+
+// generates the file anme
+static void generateBackupFileName(const char* fileName,size_t size) {
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    strftime(fileName, size, "./storage/backup/backup_%Y%m%d_%H%M%S.dat", t);
 }
